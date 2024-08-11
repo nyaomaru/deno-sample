03b0e4c6f3da41ce5bf462a95b1fc68f308092e4
nyaomaru <nyaonyao0725@gmail.com>
2024-08-11 10:49:59 +0900

    refactor: refactor

--- src/routes/greet/_middleware.ts
+++ src/routes/greet/_middleware.ts
@@ -1,10 +1,10 @@
 import { FreshContext } from "$fresh/server.ts";
 
 export async function handler(_req: Request, ctx: FreshContext) {
-    const currentName = ctx.params.name;
-    if (currentName === "Bob") {
-        ctx.params.name = "super-bob";
-    }
-    const resp = await ctx.next();
-    return resp;
+  const currentName = ctx.params.name;
+  if (currentName === "Bob") {
+    ctx.params.name = "super-bob";
+  }
+  const resp = await ctx.next();
+  return resp;
 }
--- src/routes/partials/about/[id].tsx
+++ src/routes/partials/about/[id].tsx
@@ -2,34 +2,34 @@ import { defineRoute, RouteConfig } from "$fresh/server.ts";
 import { Partial } from "$fresh/runtime.ts";
 
 const contents = [
-    "This is the documentation page. It should contain all the \n information you need to get started with the project.",
-    "bla bla bla",
+  "This is the documentation page. It should contain all the \n information you need to get started with the project.",
+  "bla bla bla",
 ];
 
 const loadContent = async (id: string) => {
-    const currentContent = contents[Number(id) % 2];
+  const currentContent = contents[Number(id) % 2];
 
-    return (
-        <article class="prose lg:prose-xl">
-            <h1>Documentation: {id}</h1>
-            <p>
-                {currentContent}
-            </p>
-        </article>
-    );
+  return (
+    <article class="prose lg:prose-xl">
+      <h1>Documentation: {id}</h1>
+      <p>
+        {currentContent}
+      </p>
+    </article>
+  );
 };
 
 export const config: RouteConfig = {
-    skipAppWrapper: true,
-    skipInheritedLayouts: true,
+  skipAppWrapper: true,
+  skipInheritedLayouts: true,
 };
 
 export default defineRoute(async (req, ctx) => {
-    const content = await loadContent(ctx.params.id);
+  const content = await loadContent(ctx.params.id);
 
-    return (
-        <Partial name="about-content">
-            {content}
-        </Partial>
-    );
+  return (
+    <Partial name="about-content">
+      {content}
+    </Partial>
+  );
 });
