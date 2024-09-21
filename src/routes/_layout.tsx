c71e43be4759fdb47f12a8205e5ebd104e6aafc4
nyaomaru <nyaonyao0725@gmail.com>
2024-09-22 08:49:24 +0900

    fix: margin

--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -2,7 +2,7 @@ import { PageProps } from "$fresh/server.ts";
 
 export default function Layout({ Component }: PageProps) {
   return (
-    <div class="px-4 py-8 mx-auto bg-cyan-300 h-screen">
+    <div class="px-4 py-8 mx-auto bg-black h-screen">
       <Component />
     </div>
   );
